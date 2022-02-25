534d14daf4758d4755adb69d00145833f043cff4
earl-stephens <stephensearl@hotmail.com>
2022-02-24 22:58:22 -0700

    Demonstrate checked exception

--- src/application/App.java
+++ src/application/App.java
@@ -2,9 +2,17 @@ package application;
 
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//throws InterruptedException
+	public static void main(String[] args) throws InterruptedException {
+		// exceptions are an error handling mechanism
+		// two kinds: run time, checked
+		// checked exceptions force you to handle them
+		
+		System.out.println("one");
+		Thread.sleep(2000);
+		//sleep can be interrupted by other threads throwing an error
+		System.out.println("two");
+		
 	}
 
 }
